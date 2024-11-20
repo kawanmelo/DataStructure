4765263fa0725aad87e2f1c367208114a2342b9c
Kawan Melo <133062524+kawanmelo@users.noreply.github.com>
2024-11-20 17:08:27 -0300

    tada: First Commit

---
+++ Lista-1---Estrutura de Dados/ED-lista1N1-questao06.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+
+bool VerificaCongruencia(int a, int b, int c);
+bool VerificaCoPrimos(int a, int b);
+bool verificar_numero_carmichael(int n);
+
+/*
+  Função : Verificar se um número é de Carmichael
+  Autor : Kawan Melo - 3º período turma F 2024.1
+  Data : 29.03.2023
+  Observações: Null
+*/
+
+int main() {
+  int num = 0;
+  printf("Digite um número: ");
+  scanf("%d", num);
+  printf("Exibindo números de carmichael de 2 até %d: ", num);
+  for (int i = 2; i <= num; i++) {
+    if(verificar_numero_carmichael(num)){
+      printf("O número %d é de Carmichael", num);
+    }
+  }
+}
+
+// Função : Verificar se a e b são CoPrimos entre si.
+bool VerificaCoPrimos(int a, int b) {
+  if (a == b) {
+    return false;
+  }
+  int *divisoresA = NULL;
+  divisoresA = (int *)malloc(sizeof(int) * a);
+  int *divisoresB = NULL;
+  divisoresB = (int *)malloc(sizeof(int) * b);
+  if (divisoresA == NULL || divisoresB == NULL) {
+    printf("Erro ao alocar memória!");
+    exit(1);
+  }
+  // Calculando os divisores de a, exceto o número 1;
+  int countA = 0;
+  for (int i = 2; i <= a; i++) {
+    if (a % i == 0) {
+      divisoresA[countA] = i;
+      countA++;
+    }
+  }
+  // Calculando os divisores de b, exceto o número 1;
+  int countB = 0;
+  for (int i = 2; i <= b; i++) {
+    if (b % i == 0) {
+      divisoresB[countB] = i;
+      countB++;
+    }
+  }
+  // Verificando se há divisores em comum entre os dois números;
+  int i = 0, j = 0;
+  while (i < countA && j < countB) {
+    if (divisoresA[i] == divisoresB[j]) {
+      return false;
+    } else if (divisoresA[i] < divisoresB[j]) {
+      i++;
+    } else {
+      j++;
+    }
+  }
+
+  return true;
+}
+
+// Função : Verificar congruência entre a, b e c. ( a ≡ b (mod c) )
+bool VerificaCongruencia(int a, int b, int c){
+  return (a % c == b % c);
+}
+
+// Função : Verificar se o número é de Carmichael.
+bool verificar_numero_carmichael(int n) {
+  // Verifica se o número não é primo
+  for (int i = 2; i < n; ++i) {
+    if (n % i == 0) {
+      // Se encontrar um divisor, não é um número de Carmichael
+      return false;
+    }
+  }
+
+  // Verifica para todos os a entre 1 e n-1 se a e n são coprimos e a^(n-1) ≡ 1
+  // (mod n)
+  for (int a = 2; a < n; ++a) {
+    if (VerificaCoPrimos(a, n)) {
+      if (!VerificaCongruencia(pow(a, n - 1), 1, n)) {
+        // Se uma das condições falhar, não é um número de Carmichael
+        return false;
+      }
+    }
+  }
+  // Se passar por todas as condições, é um número de Carmichael
+  return true;
+}
---
+++ Lista-1---Estrutura de Dados/README.md
@@ -0,0 +1,20 @@
+# Trabalho 1 - Estrutura de Dados I
+Este é o Trabalho 1 da disciplina de Estrutura de Dados I, ministrada pelo Professor Edkallenn. O trabalho aborda os seguintes tópicos:
+
+## Tópicos Abordados
+- **Entrada/Saída:** Leitura e escrita de dados em diferentes formatos.
+- **Controle de Fluxo:** Estruturas condicionais e de repetição para controlar o fluxo do programa.
+- **Funções:** Criação e utilização de funções para modularizar o código.
+- **Arrays:** Declaração, manipulação e utilização de arrays para armazenar e organizar dados.
+
+## Agradecimentos
+Gostaria de expressar meus agradecimentos ao Professor Edkallenn por ministrar a disciplina e por disponibilizar o material de estudo.
+
+## Contribuições
+Sinta-se à vontade para contribuir com o repositório, enviando sugestões, correções ou melhorias.
+
+## Sobre o Autor
+- **Nome:** Kawan Guilherme Cabral de Melo
+- **Email:** kawangcmelo@gmail.com
+- **Github:** [github.com/kawanmelo](https://github.com/kawanmelo)
+
